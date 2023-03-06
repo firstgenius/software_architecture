09b6c3c3c8ad275b49a79818ee3619ed2feb497b
firstgenius <73236714+firstgenius@users.noreply.github.com>
2023-03-06 15:21:29 +0200

    Update client.py

--- client.py
+++ client.py
@@ -19,21 +19,3 @@ if __name__ == "__main__":
             print(r.text)
         except:
             print(f"Error connecting to {url}")
-        # print(r.headers)
-        # print(r.status_code)
-        # print(r.text)
-            
-
-    
-    # url = "http://127.0.0.1:8080"
-    # data = {"message": "hello4"}
-    # r = requests.post(url = url, data = data)
-    # print(r.headers)
-
-
-    # url = "http://127.0.0.1:8080/"
-    # r = requests.get(url)
-    # print(r.headers)
-    # print(r.status_code)
-    # print(r.text)
-    # print(r.json())
